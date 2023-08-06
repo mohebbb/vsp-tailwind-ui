3e4d461ee62fd2441b535488dcf2e121f4b84b1f
moheb <moalo2018@gmail.com>
2023-08-06 23:55:57 +0330

    responsive header

--- main.js
+++ main.js
@@ -10,12 +10,21 @@ import accordion from './components/accordion'
 window.Alpine = Alpine
 
 // include components
-Alpine.data('mobile_menu', mobile_menu)
-Alpine.data('popup', popup)
-Alpine.data('tabs', tabs)
-Alpine.data('sticky', sticky)
-Alpine.data('more_btn', more_btn)
-Alpine.data('accordion', accordion)
 
+// Alpine.data('mobile_menu', mobile_menu)
+// Alpine.data('popup', popup)
+// Alpine.data('tabs', tabs)
+// Alpine.data('sticky', sticky)
+// Alpine.data('more_btn', more_btn)
+// Alpine.data('accordion', accordion)
+
+Alpine.store('menubar', {
+    open: false,
+ 
+    toggle() {
+        this.open = ! this.open
+    }
+})
+ 
 
 Alpine.start()
